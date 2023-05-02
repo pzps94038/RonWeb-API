b3a5a74a678994813040c3cd3d8e05d364fec695
ron.ro <pzps94038@yahoo.com.tw>
2023-05-02 21:00:04 +0800

    fix: 開啟Swigger

--- RonWeb.API/Program.cs
+++ RonWeb.API/Program.cs
@@ -48,12 +48,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
 
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseCors("CorsPolicy");
