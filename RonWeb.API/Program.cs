c74443eb49a19cd5fbe33854d45edfafdf475c6d
ron.ro <pzps94038@yahoo.com.tw>
2023-05-02 21:44:14 +0800

    feat: 增加heroku.yml

--- RonWeb.API/Program.cs
+++ RonWeb.API/Program.cs
@@ -7,7 +7,6 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddCors(options => {
     options.AddPolicy("CorsPolicy", builder => builder
