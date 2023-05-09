66aea4c4b0cb324f75f64e5d0b3fb7b3ef47685c
ron.ro <pzps94038@yahoo.com.tw>
2023-05-09 21:59:57 +0800

    fix: 調整查詢api

--- RonWeb.API/Models/Search/KeywordeResponse.cs
+++ RonWeb.API/Models/Search/KeywordeResponse.cs
@@ -6,7 +6,7 @@ namespace RonWeb.API.Models.Search
 	public class KeywordeResponse
 	{
 		public int Total { get; set; } = 0;
-		public List<ArticleItem> Articles = new List<ArticleItem>();
+		public List<ArticleItem> Articles { get; set; } = new List<ArticleItem>();
 		public string Keyword { get; set; } = string.Empty;
     }
 }
