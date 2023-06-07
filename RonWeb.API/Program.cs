2f527f7f56368745db999c88f924297b410c6de4
ron <pzps94038@yahoo.com.tw>
2023-06-07 11:37:23 +0800

    fix: 調整流量設定

--- RonWeb.API/Program.cs
+++ RonWeb.API/Program.cs
@@ -1,3 +1,5 @@
+using AngleSharp;
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -21,6 +23,8 @@ builder.Services.AddCors(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Auto DI
 builder.Services.Scan(scan => scan
         .FromAssemblyOf<Program>()
         .AddClasses(classes =>
@@ -28,6 +32,8 @@ builder.Services.Scan(scan => scan
         .AsImplementedInterfaces()
         .WithScopedLifetime()
 );
+
+// JWT Token
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
@@ -46,6 +52,26 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     ClockSkew = TimeSpan.Zero
                 };
             });
+// 流量限制
+//需要從載入配置檔案appsettings.json
+builder.Services.AddOptions();
+//需要儲存速率限制計算器和ip規則
+builder.Services.AddMemoryCache();
+//從appsettings.json中載入常規配置，IpRateLimiting與配置檔案中節點對應
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+//從appsettings.json中載入客戶端規則
+builder.Services.Configure<ClientRateLimitPolicies>(builder.Configuration.GetSection("ClientRateLimitPolicies"));
+//從appsettings.json中載入Ip規則
+//builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+//注入計數器和規則儲存
+builder.Services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>();
+//builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+//配置（解析器、計數器金鑰生成器）
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+//swagger
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "RonWeb-API", Version = "v1" });
@@ -54,20 +80,25 @@ builder.Services.AddSwaggerGen(c =>
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
+
+// db
 builder.Services.AddDbContext<RonWebDbContext>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+// Swagger UI Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-//}
-app.UseSwagger();
-app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
-
+//啟用客戶端IP限制速率
+app.UseIpRateLimiting();
+//啟用客戶端限制
+app.UseClientRateLimiting();
 app.Run();
